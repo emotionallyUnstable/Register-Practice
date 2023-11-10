43e5f40d9196ad81e81fc9c4b71b4acf38e273c5
emotionallyUnstable <95876061+emotionallyUnstable@users.noreply.github.com>
2023-11-10 09:57:45 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//document.getElementById("sign-in");
+
+//let welcome = "sign-in";
+
+//if (welcome == true) {
+    //window.location.replace(sign.html)
+//}
+
+const redirectButton = document.getElementById("signIn");
+
+redirectButton.addEventListener("click", function() {
+    window.location.replace("sign.html");
+})
